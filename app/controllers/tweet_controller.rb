3a3be2dfaabf0c09ab27a742caf023667c1dff88
Jason Sutter <jasonsutter87@gmail.com>
2016-07-14 16:26:32 -0700

    created tweet controller

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,27 @@
+get '/tweets' do
+	@tweet = Tweet.all
+	erb :'/tweet/index'
+end
+
+
+get '/tweets/new' do
+	erb :'/tweet/new'
+end
+
+
+post '/tweets' do
+end
+
+
+get '/tweets/:id' do
+end
+
+get '/tweets/:id/edit' do
+	erb :'/tweet/edit'
+end
+
+put '/tweets/:id' do
+end
+
+delete '/tweets/:id' do
+ent
