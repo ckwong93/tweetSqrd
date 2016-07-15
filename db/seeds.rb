59fc5e2875d82969b4221b03d17426cb5c963d8f
Jason Sutter <jasonsutter87@gmail.com>
2016-07-15 13:38:20 -0700

    created a seed file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+#users
+20.times do
+	User.create(name: Faker::Name.name ,email: Faker::Internet.email, password: '123')
+end
+
+#tweets
+100.times do
+	Tweet.create(user_id: rand(1..15), content: Faker::Lorem.sentence)
+end
+
+
+#friends
+50.times do
+	Friend.create(follower_id: rand(1..15), followee_id: rand(1..15))
+end
